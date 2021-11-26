abaa2db412d82015ff52866a65b1df371440753c
Filip Stamenković <93558411+FilipStamenkovicc@users.noreply.github.com>
2021-11-26 12:56:40 +0100

    Izmjena SQL sintaksne baze

--- dostavna.sql
+++ dostavna.sql
@@ -9,33 +9,55 @@ create table kupac(
     ulica varchar(50),
     grad varchar(50),
     postanskibroj nvarchar(50),
-    brojtelefona varchar(50)
+    drzava varchar(10),
+    brojtelefona varchar(15),
+    iban varchar(20),
+    OIB char(11),
+    email varchar(20)
 );
 
 create table narudzba(
-    brojnarudzbe int not null primary key auto_increment,
+    narudzba_id int not null primary key auto_increment,
     kupac_id int not null,
+    zaposlenik_id int not null,
     ulica varchar(50),
     grad varchar(50),
     drzava varchar(50),
     postanskibroj nvarchar(50),
     datumnarudzbe datetime,
     datumisporuke datetime,
-    kolicina nvarchar(100),
-    proizvod_id int not null
+    nacinplacanja varchar(10),
+    kolicina nvarchar(3)
 );
 
 create table proizvod(
     proizvod_id int not null primary key auto_increment,
     naziv varchar(50),
-    cijena decimal(18.2),
-    tip varchar(50),
-    brojnarudzbe int not null
+    cijena decimal(10.2),
+    tip varchar(50)
 );
 
+create table detaljinarudzbe(
+    narudzba_id int not null,
+    proizvod_id int not null,
+    kolicina char(10),
+    popust varchar(4)
+);
 
+create table zaposlenik(
+    zaposlenik_id int not null primary key auto_increment,
+    ime varchar(10) NOT NULL,
+    prezime varchar(18) NOT NULL,
+    adresa varchar(20) NOT NULL,
+    email varchar(15) NOT NULL,
+    iban varchar(20),
+    oib char(11)
+);
+
+alter table detaljinarudzbe add foreign key (narudzba_id) references narudzba(narudzba_id);
+alter table detaljinarudzbe add foreign key (proizvod_id) references proizvod(proizvod_id);
 alter table narudzba add foreign key (kupac_id) references kupac(kupac_id);
-alter table proizvod add foreign key (brojnarudzbe) references narudzba(brojnarudzbe);
+alter table narudzba add foreign key (zaposlenik_id) references zaposlenik(zaposlenik_id);
 
 
 
